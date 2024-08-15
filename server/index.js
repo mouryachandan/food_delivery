32994a3ba688b32fc2a5c480cb2fd6712d33e7b1
chandan kumar <ck43663@gmail.com>
2024-08-16 00:01:18 +0530

    mongodb config

--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./routes/User.js";
 import FoodRoutes from "./routes/Food.js";
-dotenv.config();
+dotenv.config({
+  path: ".env"
+});
 
 const app = express();
 app.use(cors({
-  origin: "*"
+  origin: "https://food-delivery-chandan-kumars-projects-b673cdc0.vercel.app",
+  credentials: true
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
