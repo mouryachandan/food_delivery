7346a6499f45da7e6eb155ffd1472d5d10d478c2
chandan kumar <ck43663@gmail.com>
2024-08-16 00:25:12 +0530

    cors policy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 const app = express();
 app.use(cors({
-  origin: "https://food-delivery-chandan-kumars-projects-b673cdc0.vercel.app",
+  origin: "*",
   credentials: true
 }));
 app.use(express.json({ limit: "50mb" }));
